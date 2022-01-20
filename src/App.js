f5e36712e6015209f26c82877069bc35d5fc6e32
Максим Лисянский <max_lisyanskiy@mail.ru>
2022-01-21 00:50:41 +0300

    finished project and published

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AppHome from "./components/AppHome/AppHome";
 import AppAbout from "./components/AppAbout/AppAbout";
 import AppCommercialProjects from "./components/AppCommercialProjects/AppCommercialProjects";
 import AppContact from "./components/AppContact/AppContact";
+import AppPetProjects from "./components/AppPetProjects/AppPetProjects";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <AppHome/>
       <AppAbout/>
       <AppCommercialProjects/>
+      <AppPetProjects/>
       <AppContact/>
     </>
   );
--- src/components/AppPetProjects/AppPetProjects.jsx
+++ src/components/AppPetProjects/AppPetProjects.jsx
@@ -1,66 +1,26 @@
 import React, {useState} from "react"
 import Fade from "react-reveal/Fade"
 
-import Modal from "../Modal/Modal"
-import Card from "./AppPetCard"
-
-import eProjects from './petProjects'
-
 const AppPetProjects = () => {
 
-    const [isModal, setModal] = React.useState(false)
-
-    const [dataModal, setDataModal] = useState(eProjects);
-    const [selectedProj, setSelectedProj] = useState(null);
-
-    const onClose = () => setModal(false)
-
-    function openModal(i) {
-      setSelectedProj(dataModal[i]);
-      setModal(true)
-    }
-
     return (
-        <div className="section" id="projects">
+        <div className="section">
             <div className="container">
                 <div className="work-wrapper">
-                    <Fade bottom>
-                        <h2>Projects</h2>
-                    </Fade>
+                    <Fade bottom cascade>
+                      <h3 className="projects-title">Do you want to see the pet projects?</h3>
+                      <div className="button">
+                        <a href="https://github.com/MaxLisyanskiy?tab=repositories" target="_blank" rel="noreferrer" className="button__text">Click</a>
 
-                    <div>
-                        <h3 className="projects-title">Commercial projects</h3>
-                        <div className="grid">
-                            <Fade bottom cascade>
-                                {eProjects.map((project, index) => (
-                                    <Card
-                                        key={index}
-                                        index={index}
-                                        heading={project.title}
-                                        paragraph={project.para}
-                                        imgUrl={project.imageSrc}
-                                        openModal={openModal}
-                                        technologyStack={project.technologyStack}
-                                    />
-                                ))}
-                            </Fade>
+                        <div className="button__wrapper">
+                          <div className="button__arrow"></div>
+                          <div className="button__border-circle"></div>
+                          <div className="button__mask-circle">
+                            <div className="button__small-circle"></div>
+                          </div>
                         </div>
-                    </div>
-
-                  { isModal 
-                    ?                     
-                    <Modal
-                      visible={isModal}
-                      title={selectedProj.title}
-                      content={<p>Что-то важное</p>}
-                      footer={<button onClick={onClose}>Закрыть</button>}
-                      onClose={onClose}
-                    >
-                      
-                    </Modal>
-                    :
-                    <Modal></Modal>
-                }
+                      </div>
+                    </Fade>
                 </div>
             </div>
         </div>
